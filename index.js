a6f257c3562c998cd5ae36fd6a0e8b26ee142c4f
Vova <tarabanvv@mail.ru>
2023-11-17 23:21:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+	.connect(
+		'mongodb+srv://admin:wwwww@cluster0.e9mf1j1.mongodb.net/?retryWrites=true&w=majority'
+	)
+	.then(() => {
+		console.log('DB ok');
+	})
+	.catch(err => {
+		console.log('DB err', err);
+	});
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+	console.log(req.body);
+
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullName: 'Вася Пупкин'
+		},
+		'secret123'
+	);
+
+	res.json({
+		success: true,
+		token
+	});
+});
+
+app.listen(4444, err => {
+	if (err) {
+		return console.log(err);
+	}
+
+	console.log('Server OK');
+});
